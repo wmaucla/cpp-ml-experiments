2befc17ee135c684c9eec2e892603ffa0dbba0ab
wmaucla <zhengweima@gmail.com>
2025-02-12 19:38:44 -0500

    adding in some more code

--- rust-xgboost/src/main.rs
+++ rust-xgboost/src/main.rs
@@ -29,5 +29,22 @@ fn main() {
 
     let bst = Booster::train(&training_params).unwrap();
 
+    // Get predictions probabilities for given matrix (as ndarray::Array1)
+    let preds = bst.predict(&dtest).unwrap();
+    
     println!("{:?}", bst.predict(&dtest).unwrap());
+
+    // Get predicted labels for each test example (0.0 or 1.0 in this case)
+    let labels = dtest.get_labels().unwrap();
+
+    // Print error rate
+    let mut num_errors = 0;
+    for (pred, label) in preds.iter().zip(labels) {
+        let pred = if *pred > 0.5 { 1.0 } else { 0.0 };
+        if pred != *label {
+            num_errors += 1;
+        }
+    }
+    println!("error={} ({}/{} correct)",
+             num_errors as f32 / preds.len() as f32, preds.len() - num_errors, preds.len());
 }
